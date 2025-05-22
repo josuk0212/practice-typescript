3037782e82ce635da9cef750f294e6708ff480d2
JSLee <josuk0212@gmail.com>
2025-05-22 21:23:44 +0900

    feat: section2-1 원시타입과 리터럴타입

---
+++ section2/src/chapter1.ts
@@ -0,0 +1,31 @@
+// number
+let num1: number = 123;
+let num2: number = -123;
+let num3: number = 0.123;
+let num4: number = -0.123;
+let num5: number = Infinity;
+let num6: number = -Infinity;
+let num7: number = NaN;
+
+// string
+let str1: string = "hello"
+let str2: string = 'hello'
+let str3: string = `hello`
+let str4: string = `hello ${num1}`
+
+// boolean
+let bool1: boolean = true;
+let bool2: boolean = false;
+
+// null
+let null1: null = null;
+
+// undefined
+let unde1: undefined = undefined;
+
+let numA: number = null; // 컴파일 옵션을 조정하면 null값 부여 가능
+
+// 리터럴 타입(값을 그냥 지정 후 재할당 불가)
+let numB: 10 = 10;
+let strA: "hi" = "hi";
+let boolA: true = true;
